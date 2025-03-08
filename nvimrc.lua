3601e555c1ffc6a247910ac98424436618c38979
lukasozpaker <lukas.ozpaker@gmail.com>
2025-03-08 01:11:20 -0500

    nvim default tab size = 4

--- nvimrc.lua
+++ nvimrc.lua
@@ -3,3 +3,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz") -- center on scroll down
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
