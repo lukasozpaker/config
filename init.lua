84363b0e64177f0af124e78e52126ae350d6a735
lukasozpaker <lukas.ozpaker@gmail.com>
2026-03-24 20:37:33 -0700

    Added nvim git support and tmux mouse toggle

--- init.lua
+++ init.lua
@@ -40,6 +40,27 @@ require("lazy").setup({
       },
     },
 
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			numhl = true,
+			signcolumn = false,
+			on_attach = function(bufnr)
+				local gs = require('gitsigns')
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+				end
+				map('n', ']c', gs.next_hunk, 'Next hunk')
+				map('n', '[c', gs.prev_hunk, 'Prev hunk')
+				map('n', '<leader>hp', gs.preview_hunk, 'Preview hunk')
+				map('n', '<leader>hi', gs.preview_hunk_inline, 'Preview inline')
+				map('n', '<leader>hd', gs.diffthis, 'Diff against index')
+				map('n', '<leader>hr', gs.reset_hunk, 'Reset hunk')
+				map('n', '<leader>hb', gs.blame_line, 'Blame line')
+			end
+		}
+	},
+
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
